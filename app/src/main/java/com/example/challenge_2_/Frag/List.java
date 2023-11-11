2bc2b0dff5611dbe4ebd02a668a6f5c9b05c85ad
TomTFSG <tommatiasferreira@gmail.com>
2023-11-11 16:14:36 +0000

    Upload de notas offline

--- app/src/main/java/com/example/challenge_2_/Frag/List.java
+++ app/src/main/java/com/example/challenge_2_/Frag/List.java
@@ -4,6 +4,8 @@ import static android.content.ContentValues.TAG;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -24,14 +26,23 @@ import android.widget.LinearLayout;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import com.example.challenge_2_.FeedReaderDbHelper;
 import com.example.challenge_2_.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class List extends Fragment {
+    private java.util.List<String> Notes;
+    private java.util.List<String> Titles;
+    int backedupnotes=0;
     String user;
     FirebaseFirestore db;
 
@@ -50,7 +61,7 @@ public class List extends Fragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         Button logout = view.findViewById(R.id.logout);
-        FloatingActionButton newNote = view.findViewById(R.id.add);
+        Button newNote = view.findViewById(R.id.add);
 
         logout.setOnClickListener(view1 -> gotoFrag(new Login()));
 
@@ -147,13 +158,80 @@ public class List extends Fragment {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     });
+            if(backedupnotes>0) {
+                for (int a = 0; a < backedupnotes; a++) {
+                    String createNote = Notes.get(a);
+                    String newTitle = Titles.get(a);
+                    Map<String, Object> note = new HashMap<>();
+                    note.put("note", createNote);
+                    note.put("title", newTitle);
+                    note.put("users_username", user);
+                    db.collection("notes")
+                            .add(note) // Firestore will generate a unique document ID AKA path to file
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Context context = getActivity().getApplicationContext();
+                                    CharSequence err = "Notes created offline have been uploaded";
+                                    int dur = Toast.LENGTH_SHORT;
+                                    Toast inc = Toast.makeText(context, err, dur);
+                                    inc.show();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error writing document", e);
+                                }
+                            });
+                }
+                //POSSIVEIS ERROS AQUI
+                FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(getContext());
+                SQLiteDatabase sql = dbHelper.getReadableDatabase();
+                sql.execSQL(FeedReaderDbHelper.SQL_DELETE_ENTRIES);
+                sql.execSQL(FeedReaderDbHelper.SQL_CREATE_ENTRIES);
+
+            }
         }
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //BACKUP WHAT WAS OFFLINE
+        FeedReaderDbHelper dbHelper = new FeedReaderDbHelper(getContext());
+        SQLiteDatabase sql = dbHelper.getReadableDatabase();
 
+        String[] projection = {
+                FeedReaderDbHelper.COLUMN_NAME_TITLE,
+                FeedReaderDbHelper.COLUMN_NAME_NOTE
+        };
+
+        String sortOrder = FeedReaderDbHelper.COLUMN_NAME_NOTE + " DESC";
+
+        Cursor cursor = sql.query(
+                FeedReaderDbHelper.TABLE_NAME,
+                projection,
+                null, // Remove the selection and selectionArgs
+                null,
+                null,
+                null,
+                sortOrder
+        );
+
+        Titles = new ArrayList<>();
+        Notes = new ArrayList();
+        while (cursor.moveToNext()) {
+            String title = cursor.getString(cursor.getColumnIndexOrThrow(FeedReaderDbHelper.COLUMN_NAME_TITLE));
+            String note = cursor.getString(cursor.getColumnIndexOrThrow(FeedReaderDbHelper.COLUMN_NAME_NOTE));
+            Titles.add(title);
+            Notes.add(note);
+
+            // Log the retrieved data for debugging
+            Log.d(TAG, "Title: " + title + ", Note: " + note);
+            backedupnotes++;
+        }
+        cursor.close();
 }
 
 
