084434650e1d223b9ca1bea94ad36a10675b4c6b
TomTFSG <tommatiasferreira@gmail.com>
2023-11-11 12:46:30 +0000

    Pesquisa v.1

--- app/src/main/java/com/example/challenge_2_/Frag/List.java
+++ app/src/main/java/com/example/challenge_2_/Frag/List.java
@@ -6,28 +6,26 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 import com.example.challenge_2_.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
 
 public class List extends Fragment {
     String user;
     FirebaseFirestore db;
+
     public List(String username) {
         user = username;
     }
@@ -39,8 +37,9 @@ public class List extends Fragment {
         return inflater.inflate(R.layout.fragment_list, container, false);
 
     }
+
     @Override
-    public void onViewCreated(View view,Bundle savedInstanceState){
+    public void onViewCreated(View view, Bundle savedInstanceState) {
         Button logout = view.findViewById(R.id.logout);
         FloatingActionButton newNote = view.findViewById(R.id.add);
 
@@ -50,26 +49,101 @@ public class List extends Fragment {
             String vazio = "";
             gotoFrag(new Edit(vazio, user, vazio, vazio));
         });
+        if(isAdded() && getView() != null) {
+            EditText editText = view.findViewById(R.id.searchbar);
+
+            editText.setOnKeyListener((v, keyCode, event) -> {
+                // Check if the key event is the Enter key
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    String search=editText.getText().toString();
+                    pesquisa(search);
+                    return true; // Consume the event
+                }
+                return false; // Let the system handle other key events
+            });
+
+            //ir buscar as notas do utilizador em questão
+            db = FirebaseFirestore.getInstance();
+            db.collection("notes")
+                            .
+
+                    whereEqualTo("users_username", user)
+                            .
+
+                    get()
+                            .
+
+                    addOnCompleteListener(task ->
+
+                    {
+
+                        LinearLayout menu = view.findViewById(R.id.menu);
+                        Context context = getContext();
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                // DADO ESTE MODELO, NENHUM UTILIZADOR PODE TER DUAS NOTAS COM O MESMO NOME
+                                // DADO ESTE MODELO, NENHUM UTILIZADOR PODE TER DUAS NOTAS COM O MESMO NOME
+                                // DADO ESTE MODELO, NENHUM UTILIZADOR PODE TER DUAS NOTAS COM O MESMO NOME
+                                String titulo = document.getString("title");
+                                String nota = document.getString("note");
+                                String id = document.getId().toString();
+                                Button select = new Button(context);
+
+                                ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(
+                                        ViewGroup.LayoutParams.MATCH_PARENT, // Width
+                                        ViewGroup.LayoutParams.WRAP_CONTENT   // Height (you can adjust this as needed)
+                                );
+
+                                select.setLayoutParams(layoutParams);
+                                select.setText(titulo);
+                                select.setTextAlignment(view.TEXT_ALIGNMENT_TEXT_START);
+
+                                select.setOnClickListener(view3 -> gotoFrag(new Edit(titulo, user, nota, id)));
+                                menu.addView(select);
+                                menu.invalidate();
+
+
+                            }
+
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    });
+        }
     }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+}
 
-        //ir buscar as notas do utilizador em questão
+
+
+    //Função criada para facilitar a visialuzação do código (troca de fragments)
+    private void gotoFrag(@NonNull androidx.fragment.app.Fragment Frag){
+        getActivity().getSupportFragmentManager().beginTransaction()
+                .replace(R.id.framelayout, Frag,null)
+                .addToBackStack(null)
+                .commit();
+    }
+    private void pesquisa(String s){
+        String search=s;
+        View view=getView();
+        LinearLayout menu = view.findViewById(R.id.menu);
+        menu.removeAllViews();
         db = FirebaseFirestore.getInstance();
         db.collection("notes")
                 .whereEqualTo("users_username", user)
+                .whereEqualTo("title",search)
                 .get()
                 .addOnCompleteListener(task -> {
-                    View view = getView();
-                    LinearLayout menu = view.findViewById(R.id.menu);
                     Context context = getContext();
                     if (task.isSuccessful()) {
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             // DADO ESTE MODELO, NENHUM UTILIZADOR PODE TER DUAS NOTAS COM O MESMO NOME
-                            // DADO ESTE MODELO, NENHUM UTILIZADOR PODE TER DUAS NOTAS COM O MESMO NOME
-                            // DADO ESTE MODELO, NENHUM UTILIZADOR PODE TER DUAS NOTAS COM O MESMO NOME
+
                             String titulo = document.getString("title");
                             String nota = document.getString("note");
                             String id = document.getId().toString();
@@ -90,18 +164,11 @@ public class List extends Fragment {
 
 
                         }
+
                     } else {
                         Log.d(TAG, "Error getting documents: ", task.getException());
                     }
                 });
-    }
 
-
-    //Função criada para facilitar a visialuzação do código (troca de fragments)
-    private void gotoFrag(@NonNull androidx.fragment.app.Fragment Frag){
-        getActivity().getSupportFragmentManager().beginTransaction()
-                .replace(R.id.framelayout, Frag,null)
-                .addToBackStack(null)
-                .commit();
     }
 }
